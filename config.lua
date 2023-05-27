bfe2c58230d8b3a10f7490df966b0e13909a3134
Santiago Zaldivar <santiago.zaldivar.1997@gmail.com>
2023-05-27 18:55:55 +1000

    find files and grep string with control key

--- config.lua
+++ config.lua
@@ -18,6 +18,9 @@ lvim.builtin.which_key.vmappings["d"] = {
   '"_d', "Delete without yanking"
 }
 
+lvim.keys.normal_mode["<c-p>"] = ":lua require'telescope.builtin'.find_files()<CR>"
+lvim.keys.normal_mode["<c-f>"] = ":lua require'telescope.builtin'.grep_string()<CR>"
+
 lvim.keys.normal_mode["gt"] = ":lua vim.lsp.buf.type_definition()<CR>"
 
 -- Non leader remaps
@@ -59,25 +62,6 @@ lvim.plugins = {
   -- Visual
   {
     "mrjones2014/nvim-ts-rainbow",
-  },
-  {
-    "karb94/neoscroll.nvim",
-    event = "WinScrolled",
-    config = function()
-      require('neoscroll').setup({
-        -- All these keys will be mapped to their corresponding default scrolling animation
-        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
-          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-        hide_cursor = true,          -- Hide cursor while scrolling
-        stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-        respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing_function = nil,       -- Default easing function
-        pre_hook = nil,              -- Function to run before the scrolling animation starts
-        post_hook = nil,             -- Function to run after the scrolling animation ends
-      })
-    end
   },
   { 'folke/tokyonight.nvim' }
 }
